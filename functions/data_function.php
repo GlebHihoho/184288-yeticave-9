9f804b6eb020a68ebe679e2e32b03e7f6da7e735
GlebHihoho <gleb.motuz@mail.ru>
2019-05-13 23:43:24 +0300

    fixes

--- functions/data_function.php
+++ functions/data_function.php
@@ -1,49 +1,47 @@
 <?php
-
-function get_link()
+class DbConnection
 {
-    $link = mysqli_connect('localhost', 'root', '12321Glrbct', 'yeticave');
-    mysqli_set_charset($link, "utf8");
+    private static $connection;
 
-    return $link;
+    static function getConnection()
+    {
+        if (self::$connection === null) {
+            self::$connection = mysqli_connect('localhost', 'root', '12321Glrbct', 'yeticave');
+            if (!self::$connection) {
+                die("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
+            }
+            mysqli_set_charset(DbConnection::$connection, "utf8");
+        }
+        return self::$connection;
+    }
 }
 
 function get_categories() : array
 {
-    $link = get_link();
+    $link = DbConnection::getConnection();
+    $sql_request = 'SELECT * FROM categories';
+    $get_categories = mysqli_query($link, $sql_request);
+    $categories = mysqli_fetch_all($get_categories, MYSQLI_ASSOC);
 
-    if (!$link) {
-        print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-    } else {
-        $sql_request = 'SELECT * FROM categories';
-        $get_categories = mysqli_query($link, $sql_request);
-        $categories = mysqli_fetch_all($get_categories, MYSQLI_ASSOC);
-
-        return $categories;
-    }
+    return $categories;
 }
 
 function get_all_lots() : array
 {
-    $link = get_link();
+    $link = DbConnection::getConnection();
+    $sql_request =
+        'SELECT
+            lots.id,
+            lots.name as lot_name,
+            lots.start_price,
+            lots.img_url,
+            lots.time_end,
+            categories.name as category_name
+        FROM lots
+        JOIN categories ON categories.id = lots.id
+        ORDER BY lots.time_end ASC;';
+    $get_lots = mysqli_query($link, $sql_request);
+    $lots = mysqli_fetch_all($get_lots, MYSQLI_ASSOC);
 
-    if (!$link) {
-        print("Ошибка: Невозможно подключиться к MySQL " . mysqli_connect_error());
-    } else {
-        $sql_request =
-            'SELECT
-                lots.id,
-                lots.name as lot_name,
-                lots.start_price,
-                lots.img_url,
-                lots.time_end,
-                categories.name as category_name
-            FROM lots
-            JOIN categories ON categories.id = lots.id
-            ORDER BY lots.time_end ASC;';
-        $get_lots = mysqli_query($link, $sql_request);
-        $lots = mysqli_fetch_all($get_lots, MYSQLI_ASSOC);
-
-        return $lots;
-    }
+    return $lots;
 }
